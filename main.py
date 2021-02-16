de30bd2cec160c2cd4945bcd530964e7db9496b7
Nirajan <gnirajan46@gmail.com>
2021-02-16 11:37:21 +0545

    slide changes

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+
+a='soft'
+b='warica'
+print(f' {b[-1::-1]}{a[-1::-1]} is my college')
+
+name='shoobham gadeula'
+print(name[-5:0:-1])
+
+
+'''
+'''
+number=int(input("enter number "))
+
+if number % 2 == 0:
+    print('number is even')
+else:
+    print("number is odd")
+'''
+
+
+
+
+
